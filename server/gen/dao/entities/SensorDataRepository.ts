a26be64795a622028f4ad5049b5d6e519c7c47b1
MrGoblings <mrgoblings@gmail.com>
2024-03-14 11:33:30 +0000

    Added edm

---
+++ server/gen/dao/entities/SensorDataRepository.ts
@@ -0,0 +1,189 @@
+import { query } from "sdk/db";
+import { producer } from "sdk/messaging";
+import { extensions } from "sdk/extensions";
+import { dao as daoApi } from "sdk/db";
+
+export interface SensorDataEntity {
+    readonly Id: number;
+    tester?: string;
+}
+
+export interface SensorDataCreateEntity {
+    readonly tester?: string;
+}
+
+export interface SensorDataUpdateEntity extends SensorDataCreateEntity {
+    readonly Id: number;
+}
+
+export interface SensorDataEntityOptions {
+    $filter?: {
+        equals?: {
+            Id?: number | number[];
+            tester?: string | string[];
+        };
+        notEquals?: {
+            Id?: number | number[];
+            tester?: string | string[];
+        };
+        contains?: {
+            Id?: number;
+            tester?: string;
+        };
+        greaterThan?: {
+            Id?: number;
+            tester?: string;
+        };
+        greaterThanOrEqual?: {
+            Id?: number;
+            tester?: string;
+        };
+        lessThan?: {
+            Id?: number;
+            tester?: string;
+        };
+        lessThanOrEqual?: {
+            Id?: number;
+            tester?: string;
+        };
+    },
+    $select?: (keyof SensorDataEntity)[],
+    $sort?: string | (keyof SensorDataEntity)[],
+    $order?: 'asc' | 'desc',
+    $offset?: number,
+    $limit?: number,
+}
+
+interface SensorDataEntityEvent {
+    readonly operation: 'create' | 'update' | 'delete';
+    readonly table: string;
+    readonly entity: Partial<SensorDataEntity>;
+    readonly key: {
+        name: string;
+        column: string;
+        value: number;
+    }
+}
+
+export class SensorDataRepository {
+
+    private static readonly DEFINITION = {
+        table: "SENSORDATA",
+        properties: [
+            {
+                name: "Id",
+                column: "SENSORDATA_ID",
+                type: "INTEGER",
+                id: true,
+                autoIncrement: true,
+            },
+            {
+                name: "tester",
+                column: "SENSORDATA_TESTER",
+                type: "VARCHAR",
+            }
+        ]
+    };
+
+    private readonly dao;
+
+    constructor(dataSource?: string) {
+        this.dao = daoApi.create(SensorDataRepository.DEFINITION, null, dataSource);
+    }
+
+    public findAll(options?: SensorDataEntityOptions): SensorDataEntity[] {
+        return this.dao.list(options);
+    }
+
+    public findById(id: number): SensorDataEntity | undefined {
+        const entity = this.dao.find(id);
+        return entity ?? undefined;
+    }
+
+    public create(entity: SensorDataCreateEntity): number {
+        const id = this.dao.insert(entity);
+        this.triggerEvent({
+            operation: "create",
+            table: "SENSORDATA",
+            entity: entity,
+            key: {
+                name: "Id",
+                column: "SENSORDATA_ID",
+                value: id
+            }
+        });
+        return id;
+    }
+
+    public update(entity: SensorDataUpdateEntity): void {
+        this.dao.update(entity);
+        this.triggerEvent({
+            operation: "update",
+            table: "SENSORDATA",
+            entity: entity,
+            key: {
+                name: "Id",
+                column: "SENSORDATA_ID",
+                value: entity.Id
+            }
+        });
+    }
+
+    public upsert(entity: SensorDataCreateEntity | SensorDataUpdateEntity): number {
+        const id = (entity as SensorDataUpdateEntity).Id;
+        if (!id) {
+            return this.create(entity);
+        }
+
+        const existingEntity = this.findById(id);
+        if (existingEntity) {
+            this.update(entity as SensorDataUpdateEntity);
+            return id;
+        } else {
+            return this.create(entity);
+        }
+    }
+
+    public deleteById(id: number): void {
+        const entity = this.dao.find(id);
+        this.dao.remove(id);
+        this.triggerEvent({
+            operation: "delete",
+            table: "SENSORDATA",
+            entity: entity,
+            key: {
+                name: "Id",
+                column: "SENSORDATA_ID",
+                value: id
+            }
+        });
+    }
+
+    public count(options?: SensorDataEntityOptions): number {
+        return this.dao.count(options);
+    }
+
+    public customDataCount(): number {
+        const resultSet = query.execute('SELECT COUNT(*) AS COUNT FROM "SENSORDATA"');
+        if (resultSet !== null && resultSet[0] !== null) {
+            if (resultSet[0].COUNT !== undefined && resultSet[0].COUNT !== null) {
+                return resultSet[0].COUNT;
+            } else if (resultSet[0].count !== undefined && resultSet[0].count !== null) {
+                return resultSet[0].count;
+            }
+        }
+        return 0;
+    }
+
+    private async triggerEvent(data: SensorDataEntityEvent) {
+        const triggerExtensions = await extensions.loadExtensionModules("server-entities-SensorData", ["trigger"]);
+        triggerExtensions.forEach(triggerExtension => {
+            try {
+                triggerExtension.trigger(data);
+            } catch (error) {
+                console.error(error);
+            }            
+        });
+        producer.topic("server-entities-SensorData").send(JSON.stringify(data));
+    }
+}
---
+++ server/gen/ui/entities/SensorData/controller.js
@@ -0,0 +1,157 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'server.entities.SensorData';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/server/gen/api/entities/SensorDataService.ts";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', function ($scope, $http, messageHub, entityApi) {
+
+		//-----------------Custom Actions-------------------//
+		$http.get("/services/js/resources-core/services/custom-actions.js?extensionPoint=server-custom-action").then(function (response) {
+			$scope.pageActions = response.data.filter(e => e.perspective === "entities" && e.view === "SensorData" && (e.type === "page" || e.type === undefined));
+			$scope.entityActions = response.data.filter(e => e.perspective === "entities" && e.view === "SensorData" && e.type === "entity");
+		});
+
+		$scope.triggerPageAction = function (actionId) {
+			for (const next of $scope.pageActions) {
+				if (next.id === actionId) {
+					messageHub.showDialogWindow("server-custom-action", {
+						src: next.link,
+					});
+					break;
+				}
+			}
+		};
+
+		$scope.triggerEntityAction = function (actionId, selectedEntity) {
+			for (const next of $scope.entityActions) {
+				if (next.id === actionId) {
+					messageHub.showDialogWindow("server-custom-action", {
+						src: `${next.link}?id=${selectedEntity.Id}`,
+					});
+					break;
+				}
+			}
+		};
+		//-----------------Custom Actions-------------------//
+
+		function resetPagination() {
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 20;
+		}
+		resetPagination();
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entitySearch", function (msg) {
+			resetPagination();
+			$scope.filter = msg.data.filter;
+			$scope.filterEntity = msg.data.entity;
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber, filter) {
+			if (!filter && $scope.filter) {
+				filter = $scope.filter;
+			}
+			$scope.dataPage = pageNumber;
+			entityApi.count(filter).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("SensorData", `Unable to count SensorData: '${response.message}'`);
+					return;
+				}
+				$scope.dataCount = response.data;
+				let offset = (pageNumber - 1) * $scope.dataLimit;
+				let limit = $scope.dataLimit;
+				let request;
+				if (filter) {
+					filter.$offset = offset;
+					filter.$limit = limit;
+					request = entityApi.search(filter);
+				} else {
+					request = entityApi.list(offset, limit);
+				}
+				request.then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("SensorData", `Unable to list/filter SensorData: '${response.message}'`);
+						return;
+					}
+					$scope.data = response.data;
+				});
+			});
+		};
+		$scope.loadPage($scope.dataPage, $scope.filter);
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+		};
+
+		$scope.openDetails = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.showDialogWindow("SensorData-details", {
+				action: "select",
+				entity: entity,
+			});
+		};
+
+		$scope.openFilter = function (entity) {
+			messageHub.showDialogWindow("SensorData-filter", {
+				entity: $scope.filterEntity,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			messageHub.showDialogWindow("SensorData-details", {
+				action: "create",
+				entity: {},
+			}, null, false);
+		};
+
+		$scope.updateEntity = function (entity) {
+			messageHub.showDialogWindow("SensorData-details", {
+				action: "update",
+				entity: entity,
+			}, null, false);
+		};
+
+		$scope.deleteEntity = function (entity) {
+			let id = entity.Id;
+			messageHub.showDialogAsync(
+				'Delete SensorData?',
+				`Are you sure you want to delete SensorData? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("SensorData", `Unable to delete SensorData: '${response.message}'`);
+							return;
+						}
+						$scope.loadPage($scope.dataPage, $scope.filter);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+	}]);
