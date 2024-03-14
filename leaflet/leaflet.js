a46ed5d4519bd704d2f32f60ba400bb523a455ec
MoniMonkata126126 <simeon.k.simeonov06@gmail.com>
2024-03-15 00:58:32 +0200

    Tests for popups

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -3,7 +3,7 @@ var map = L.map('map');
 var customMarker = L.Marker.extend({
     options: {
         time: "",
-        PH: 0
+        pH: 0
     }
 });
 
@@ -12,25 +12,25 @@ var data = [{
     latitude: 43.2321,
     longitude: 23.4563,
     time: "08:09",
-    PH: 7
+    pH: 7
 },
 {
     latitude: 42.6499,
     longitude: 23.3638,
     time: "09:15",
-    PH: 8
+    pH: 8
 },
 {
     latitude: 42.6535,
     longitude: 23.3727,
     time: "12:37",
-    PH: 4
+    pH: 4
 },
 {
     latitude: 42.6698,
     longitude: 23.3836,
     time: "03:48",
-    PH: 5.6
+    pH: 5.6
 }
 ]
 
