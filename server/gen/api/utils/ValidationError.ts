2fcf47647ef71e0690ba90fb276cc0a655388347
MrGoblings <80454439+Mrgoblings@users.noreply.github.com>
2024-03-14 13:29:01 +0200

    Delete server directory

--- server/gen/api/utils/ValidationError.ts
+++
@@ -1,8 +0,0 @@
-export class ValidationError extends Error {
-    readonly name = "ValidationError";
-    readonly stack = (new Error()).stack;
-
-    constructor(message: string) {
-        super(message);
-    }
-}
