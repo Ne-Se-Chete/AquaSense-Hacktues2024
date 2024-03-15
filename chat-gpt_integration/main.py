133b78124fbf6b6b172d2dcddb2f42030b1fed8e
ValeriTodorov123 <valerytodorov5@gmail.com>
2024-03-16 01:10:19 +0200

    gpt_reaction_to_trash_pH_and_oil

--- chat-gpt_integration/main.py
+++ chat-gpt_integration/main.py
@@ -7,20 +7,34 @@ with open("key.txt", "r") as file:
 client = OpenAI(api_key=key)
 
 
-def generate_instructions(user_text):
-
-    content = (
-
-    )
-
+def generate_instructions(information, tokens):
+    text = information
+    print(tokens)
     response = client.chat.completions.create(
         model="gpt-4",
-        messages=[{"role": "user", "content": user_text}],
-        max_tokens=50,
+        messages=[{"role": "user", "content": text}],
+        max_tokens=tokens,
     )
 
     print(response.choices[0].message.content.strip())
+    return response.choices[0].message.content.strip()
 
 
-generate_instructions("what d"
-                      "oes a ph of 9 in a river mean pollution on a normal pH of 7")
+
+pH = 10
+trash = "bottle"
+oil = True
+
+instruction = "Summorize in 2 sentences what meassuring {} ph in an ocean means".format(pH)
+tokens = 60
+
+if trash != "none":
+    instruction += " what {} in the watter means".format(trash)
+    tokens += 20
+
+if oil:
+    instruction += " and what oil in the watter means"
+    tokens += 20
+
+
+generate_instructions(instruction, tokens)
