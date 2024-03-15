6c6dffb488f1cfee3573fb245f4e6b474a564207
yasenOfficial <yasentsv@gmail.com>
2024-03-15 15:45:37 +0200

    servo function

---
+++ servo.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep   
+GPIO.setmode(GPIO.BCM) 
+
+def moveServo(howmuch):
+    GPIO.setup(17,GPIO.OUT)  
+    p = GPIO.PWM(17, 50)    
+    p.start(0)           
+
+    p.ChangeDutyCycle(int(howmuch))    
+
+p.stop()          
+GPIO.cleanup()          
