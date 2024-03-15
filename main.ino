2b915849b21af1de4bf8447bd3b98343d0062112
yasenOfficial <yasentsv@gmail.com>
2024-03-15 19:15:20 +0200

    add more ino code

--- main.ino
+++ main.ino
@@ -1,22 +1,29 @@
-void setup(){
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 
+
+void setup() {
+  // Set GPIO 0, 3, and 1 as outputs
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
   Serial.begin(9600);
-  pinMode(7, OUTPUT);
+
+
+
+  // Set GPIO 0, 3, and 1 to high level
+//  digitalWrite(8, HIGH);
+
 }
 
-void loop(){
+void loop() {
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  Serial.println(analogRead(analogInPin));
 
-  if(Serial.available() > 0) {
-      String data = Serial.readStringUntil('\n');
-      Serial.print("Im arduino");
-      if (data == "HIGH")
-      {
-        digitalWrite(7, HIGH);
-      }else if (data == "LOW")
-      {
-        digitalWrite(7, LOW);
-      }else{
-        Serial.println("Invalid Input");
-      }
-  }
+  delay(250);
+
+
 }
